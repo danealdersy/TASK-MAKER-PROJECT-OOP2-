34f158cb7b45a4a68148a273b373b33e31a72718
Crizzly Iya Sabellita <crizzlyiya.sabellita@cit.edu>
2026-03-10 00:22:25 +0800

    Add files via upload

---
+++ TaskMaker/Menu.java
@@ -0,0 +1,21 @@
+class Menu {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void display() {
+        System.out.println("\n     TO-DO LIST MENU:     ");
+        System.out.println("1. Add Task");
+        System.out.println("2. View Tasks");
+        System.out.println("3. Complete Task");
+        System.out.println("4. Remove Task");
+        System.out.println("5. Exit");
+    }
+
+    public int getChoice() {
+        System.out.print("Enter choice: ");
+        while (!scanner.hasNextInt()) {
+            System.out.print("Invalid input. Enter a number: ");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+}
